e9751b819e62ff41cf323d33757a5dcb14a2ac0c
CDesiree <desireecastrocpe@gmail.com>
2024-10-12 13:58:06 +0800

    [feat] validate folder path input

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,7 +1,16 @@
 class FindInFiles {
 
   static void main(String [] args){
+    String folderPath = args[0]
 
+    File folder = new File(folderPath)
+    if(!folder.isDirectory()) {
+      println("Invalid folder path: ${folderPath}")
+
+    }else{
+      println("Valid folder path thanks~")
+      System.exit(1)
+    }
   }
 }
 
